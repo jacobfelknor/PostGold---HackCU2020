2ae7b95d5fe65304b45098e6d5798f9dbe78c3b1
Jacob Felknor <jacobfelknor073@gmail.com>
2020-02-22 15:40:16 -0700

    generate shitty titles

--- graphs.py
+++ graphs.py
@@ -1,5 +1,39 @@
-import networkx as nx
 import random
+import string
+
+import networkx as nx
+import praw
+
+from config.keys import client_id, client_secret, user_agent
+
+
+def getTitle(reddit, subreddit, numberOfPosts):
+    # continued from code above
+    titleData = {}
+    titleWords = []
+    for submission in reddit.subreddit(subreddit).top(limit=numberOfPosts):
+        title = submission.title
+        title.translate(string.punctuation)
+        titles = title.split(" ")
+        titleWords.append(titles)
+        for i in titles:
+            if titleData.get(i, -1) == -1:
+                titleData.update({i: 0})
+            titleData[i] += 1
+    return titleWords, titleData
+
+
+def getPostText(reddit, subreddit, numberOfPosts):
+    textData = {}
+    for submission in reddit.subreddit(subreddit).top(limit=numberOfPosts):
+        text = submission.selftext
+        text.translate(string.punctuation)
+        posts = text.split(" ")
+        for i in posts:
+            if textData.get(i, -1) == -1:
+                textData.update({i: 0})
+            textData[i] += 1
+    return textData
 
 
 class Node:
@@ -25,31 +59,78 @@ def search_graph(word, g):
     return None
 
 
+def build_graph(titles, freqs, g):
+    for title in titles:
+        ii = 0
+        last_node = None
+        for word in title:
+            first = False
+            end = False
+            if word == title[0]:
+                # print(word)
+                first = True
+            elif word == title[-1]:
+                end = True
+
+            if not search_graph(word, g):
+                node = Node(word=word, frequency=freqs[word], first=first, end=end)
+                g.add_node(node)
+                if first:
+                    g.graph["firsts"].append(node)
+                    node.start_freq += 1
+                elif end:
+                    g.graph["ends"].append(node)
+                    node.end_freq += 1
+            else:
+                node = search_graph(word, g)
+                if first:
+                    g.graph["firsts"].append(node)
+                    node.start_freq += 1
+                elif end:
+                    g.graph["ends"].append(node)
+                    node.end_freq += 1
+
+            if last_node:
+                g.add_edge(node, last_node)
+
+            last_node = node
+
+            ii += 1
+
+
+def generate_title(g):
+    gen_title = ""
+    num_firsts = len(g.graph["firsts"])
+    word = g.graph["firsts"][random.randrange(0, num_firsts)]
+    end = False
+    while True:
+        gen_title += word.word + " "
+        try:
+            len_edges = len(g.edges(word))
+            if len_edges == 0:
+                break
+            word = list(g.edges(word))[random.randrange(0, len_edges)][1]
+        except IndexError:
+            break
+        if word.end:
+            break
+
+    return gen_title
+
+
 if __name__ == "__main__":
-    g = nx.DiGraph()
 
-    words = ["this", "is", "a", "test", "sentence"]
-    # nodes = []
-    for x in words:
-        g.add_node(
-            Node(
-                word=x,
-                frequency=random.randint(1, 10),
-                start=bool(random.getrandbits(1)),
-                end=bool(random.getrandbits(1)),
-            )
-        )
+    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+    subreddit = "lifeofnorman"
+    numberOfPosts = 100
+    titles, freqs = getTitle(reddit, subreddit, numberOfPosts)
+    # print(titles)
+    g = nx.DiGraph(firsts=[], ends=[])
 
-    g.add_edge()
+    build_graph(titles, freqs, g)
 
-    # g.add_node(
-    #     Node(
-    #         word="this",
-    #         frequency=random.randint(1, 10),
-    #         start=bool(random.getrandbits(1)),
-    #         end=bool(random.getrandbits(1)),
-    #     )
-    # )
+    for i in range(15):
+        print(generate_title(g))
+        print("\n\n")
+    # print(list(g.edges(word))[0][])
 
-    print(g.nodes)
-    # g.add_edge()
