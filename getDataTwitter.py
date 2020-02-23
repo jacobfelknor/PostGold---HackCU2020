734388e3111b5058402c8f37f71bea97c1af7a2b
slundell123 <salu3240@colorado.edu>
2020-02-22 18:02:29 -0700

    putting twitter data into an list

--- getDataTwitter.py
+++ getDataTwitter.py
@@ -1,11 +1,33 @@
 import tweepy
+import re
 
 from config.keys import twitter_client_id, twitter_client_secret, access_token, access_token_secret
 
 
-def get_last_tweet(self):
-    tweet = self.client.user_timeline(id=self.client_id, count=1)[0]
-    print(tweet.text)
+def getTitleSentenceList(setOfTweets):
+    titles = []
+    my_title = []
+    for status in setOfTweets:
+        tweet = status._json
+        newTweet = tweet["text"]
+        if "RT @" not in newTweet and "http" not in newTweet:  # and ("https" not in tweet["text"]):
+            my_title.append(newTweet)
+            titles.append(my_title)
+            my_title = []
+    return titles
+
+
+def getTitle(tweet):
+    # continued from code above
+    titleData = {}
+    for title in tweet:
+        title = title[0].lower()
+        titles = re.sub("[^\w]", " ", title).split()
+        for i in titles:
+            if titleData.get(i, -1) == -1:
+                titleData.update({i: 0})
+            titleData[i] += 1
+    print(titleData)
 
 
 auth = tweepy.OAuthHandler(twitter_client_id, twitter_client_secret)
@@ -13,10 +35,6 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-stuff = api.user_timeline(screen_name="kanyewest", count=200, include_rts=True)
+setOfTweets = api.user_timeline(screen_name="elonmusk", count=1000, include_rts=True)
 
-for status in stuff:
-    tweet = status._json
-    if "RT @" not in tweet["text"]:  # and ("https" not in tweet["text"]):
-        print(tweet["text"])
-
+getTitle(getTitleSentenceList(setOfTweets))
