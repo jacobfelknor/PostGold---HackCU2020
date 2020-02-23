3cb9c2ea1b237db71cc90c8e28d5b65864e50792
Jacob Felknor <jacobfelknor073@gmail.com>
2020-02-23 10:12:12 -0700

    few quick changes

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
     path("reddit/", include("reddit.urls")),
     path("twitter/", include("twitter.urls")),
